4ce5eb3abc0215674e88779bd0a4832d819ac24b
Gustavo Pissai <gustavopissai3d@gmail.com>
2024-10-11 11:52:11 -0300

    Primeiro commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {};
+
+export const SpotifyConfiguration = {
+    clientId: '81d922acd1914c5781338d9fe4e1b07a',
+    authEndPoint: 'https://accounts.spotify.com/authorize',
+    redirectUrl: 'http://localhost:4200/login/',
+    scopes: [
+        "user-read-currently-playing", // musica tocando agora.
+        "user-read-recently-played", // ler musicas tocadas recentemente
+        "user-read-playback-state", // ler estado do player do usuario
+        "user-top-read", // top artistas e musicas do usuario
+        "user-modify-playback-state", // alterar do player do usuario.
+        "user-library-read", // ler biblioteca dos usuarios
+        "playlist-read-private", // ler playlists privads
+        "playlist-read-collaborative" // ler playlists colaborativas
+    ]
+}
